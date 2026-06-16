b679277f4359aa9133225d564d2455284c28ae65
Aakar <Aakarshxk@gmail.com>
2026-06-16 07:04:58 +0530

    Fix

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "**" },
     ],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
